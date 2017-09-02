287a2f30a663224b76685e33f7ce7cd78f5b0137
sanjsanj <sanjsanj@hotmail.com>
2017-09-02 08:52:10 +0100

    add user, updated at, and comments to issues

--- js/main.js
+++ js/main.js
@@ -145,6 +145,10 @@ function returnOpenIssue (issue) {
   if (issue.state === "open") return issue;
 }
 
+const issueIcon = `<svg aria-hidden="true" class="issue-icon-svg" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M11 11.28V5c-.03-.78-.34-1.47-.94-2.06C9.46 2.35 8.78 2.03 8 2H7V0L4 3l3 3V4h1c.27.02.48.11.69.31.21.2.3.42.31.69v6.28A1.993 1.993 0 0 0 10 15a1.993 1.993 0 0 0 1-3.72zm-1 2.92c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zM4 3c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v6.56A1.993 1.993 0 0 0 2 15a1.993 1.993 0 0 0 1-3.72V4.72c.59-.34 1-.98 1-1.72zm-.8 10c0 .66-.55 1.2-1.2 1.2-.65 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>`
+
+const commentIcon = `<svg aria-hidden="true" class="issue-icon-comment" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M14 1H2c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1h2v3.5L7.5 11H14c.55 0 1-.45 1-1V2c0-.55-.45-1-1-1zm0 9H7l-2 2v-2H2V2h12v8z"></path></svg>`;
+
 function setIssueLabels (issueNumber, anchorDiv) {
   fetch(`https://api.github.com/repos/ComparetheMarket/EpiServerCTM/issues/${issueNumber}/labels?access_token=${secrets.gitToken}`)
   .then(response => response.json())
@@ -155,7 +159,21 @@ function setIssueLabels (issueNumber, anchorDiv) {
   .catch(err => console.log(err))
 }
 
-const issueIcon = `<svg aria-hidden="true" class="issue-icon-svg" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M11 11.28V5c-.03-.78-.34-1.47-.94-2.06C9.46 2.35 8.78 2.03 8 2H7V0L4 3l3 3V4h1c.27.02.48.11.69.31.21.2.3.42.31.69v6.28A1.993 1.993 0 0 0 10 15a1.993 1.993 0 0 0 1-3.72zm-1 2.92c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zM4 3c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v6.56A1.993 1.993 0 0 0 2 15a1.993 1.993 0 0 0 1-3.72V4.72c.59-.34 1-.98 1-1.72zm-.8 10c0 .66-.55 1.2-1.2 1.2-.65 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"></path></svg>`
+function setComments (issueNumber, anchor) {
+  fetch(`https://api.github.com/repos/ComparetheMarket/EpiServerCTM/pulls/${issueNumber}/comments?access_token=${secrets.gitToken}`)
+  .then(response => response.json())
+  .then(comments => {
+    if (comments.length < 1) return null;
+
+    const element = createDiv(comments.length, "issue-comment-number");
+    const icon = document.createElement("div");
+    icon.innerHTML = commentIcon;
+    icon.className = "issue-comment-icon";
+
+    append(anchor, [icon, element]);
+  })
+  .catch(err => console.log(err))
+}
 
 fetch(`https://api.github.com/repos/ComparetheMarket/EpiServerCTM/pulls?state=all&access_token=${secrets.gitToken}`)
 .then(response => response.json())
@@ -163,16 +181,25 @@ fetch(`https://api.github.com/repos/ComparetheMarket/EpiServerCTM/pulls?state=al
   const openIssuesArray = data.filter(issue => returnOpenIssue(issue));
   
   openIssuesArray.map(issue => {
+    console.log(issue);
     const icon = document.createElement("div");
     icon.innerHTML = issueIcon;
     icon.className = "issue-icon";
 
     const anchor = createAnchor(issue.head.ref, issue.html_url);
     anchor.className = "issue";
+    
+    let lastUpdated = new Date(issue.updated_at);
+    lastUpdated = lastUpdated.toLocaleDateString("en-GB", dateOptions);
+
+    const info = createDiv(`${issue.user.login} - ${lastUpdated}`, "issue-info");
+
     const anchorDiv = createDiv(null, "anchor-container");
     
     setIssueLabels(issue.number, anchorDiv);
-    append(anchorDiv, [icon, anchor]);
+    setComments(issue.number, anchor)
+    
+    append(anchorDiv, [icon, anchor, info]);
     append(issuesContainer, [anchorDiv]);
   })
 })
