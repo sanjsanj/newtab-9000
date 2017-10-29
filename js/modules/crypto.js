e9b796d7b96fb178565e6ffdaf8008dde35c2a65
sanjsanj <sanjsanj@hotmail.com>
2017-10-29 16:24:00 +0000

    add BTC price

---
+++ js/modules/crypto.js
@@ -0,0 +1,10 @@
+function fetchNews () {
+  fetch("https://api.coindesk.com/v1/bpi/currentprice/gbp.json")
+  .then(response => response.json())
+  .then(data => {
+    const btcContainer = document.querySelector(".btc-container");
+    let btcGbpRate = parseInt((data.bpi.GBP.rate).replace(/,/g, ''), 10);
+    btcContainer.innerHTML += btcGbpRate;
+  })
+  .catch(err => console.log(err))
+}
--- js/modules/news.js
+++ js/modules/news.js
@@ -1,10 +1,10 @@
-const newsContainer = document.querySelector(".news-container");
-
 function fetchNews () {
   fetch(`https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=${secrets.newsApi}`)
   .then(response => response.json())
   .then(data => {
-    data.articles.map(article => {
+    const newsContainer = document.querySelector(".news-container");
+
+    data.articles.map((article) => {
       const anchor = createAnchor(article.title, article.url);
       const articleDiv = createDiv(null, "news-item")
       anchor.className = "news-item-anchor";
