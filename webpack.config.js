0c92533dc5d7c51927836d041647251cccae3e23
sanjsanj <sanjsanj@hotmail.com>
2018-01-05 18:20:00 +0000

    remove all non-essential build and dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,4 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
-
-const extractCss = new ExtractTextPlugin({
-  filename: './css/s.css',
-});
 
 module.exports = {
   entry: './js/index.js',
@@ -15,30 +10,9 @@ module.exports = {
 
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: extractCss.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                minimize: true,
-              },
-            },
-            {
-              loader: 'postcss-loader',
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
-        }),
-      },
     ],
   },
 
   plugins: [
-    extractCss,
   ],
 };
