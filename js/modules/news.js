da7d2c3a9166d12a22917ad37568e8f20ae1ebdf
sanjsanj <sanjsanj@hotmail.com>
2018-01-26 18:51:29 +0000

    change news icon source

--- js/modules/news.js
+++ js/modules/news.js
@@ -6,27 +6,27 @@ const createSourceImage = (sourceId) => {
   element.className = 'news-source-image';
   switch (sourceId) {
     case 'bbc-news':
-      element.src = 'https://icons.better-idea.org/icon?size=80..120..200&url=www.bbc.co.uk%2fnews';
+      element.src = 'http://m.files.bbci.co.uk/modules/bbc-morph-news-waf-page-meta/2.1.0/apple-touch-icon-57x57-precomposed.png';
       break;
 
     case 'google-news-uk':
-      element.src = 'https://icons.better-idea.org/icon?size=80..120..200&url=google.co.uk';
+      element.src = 'https://www.google.de/images/branding/product/ico/googleg_lodp.ico';
       break;
 
     case 'the-next-web':
-      element.src = 'https://icons.better-idea.org/icon?size=80..120..200&url=https%3a%2f%2fthenextweb.com%2f';
+      element.src = 'https://cdn0.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/favicon-16x16.png';
       break;
 
     case 'the-verge':
-      element.src = 'https://icons.better-idea.org/icon?size=80..120..200&url=theverge.com';
+      element.src = 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/7395367/favicon-16x16.0.png';
       break;
 
     case 'techcrunch':
-      element.src = 'https://icons.better-idea.org/icon?size=80..120..200&url=https%3a%2f%2ftechcrunch.com%2f';
+      element.src = 'https://s0.wp.com/wp-content/themes/vip/techcrunch-2013/assets/images/homescreen_TCIcon.png';
       break;
 
     case 'techradar':
-      element.src = 'https://icons.better-idea.org/icon?size=80..120..200&url=http%3a%2f%2fwww.techradar.com%2f';
+      element.src = 'http://cdn0.static.techradar.futurecdn.net/201801163/favicon.ico';
       break;
 
     default:
